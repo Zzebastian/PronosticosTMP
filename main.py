b5f1b3f269b4ec1fb65f22688e927b9733873c49
Replit user <>
2023-08-19 22:14:05 +0000

    Finalizado parte texto tenis

--- main.py
+++ main.py
@@ -23,39 +23,55 @@ for ref in elemLinks:
   referencias.append(rr)
 #print(referencias)
 
-url = referencias[0]
+
+# url = referencias[0]
 tag =  'table'
 Elemento = {'class' : "table table-wrap-bordered table-thead-color"}
-tabla = sopa(url, tag, Elemento)
-print(len(tabla))
+Tabla = []
+for url in referencias:
+  Juego = re.split('/',url)
+  Juego = Juego[6]
+  Juego = Juego.replace('-', ' ')
+  Juego = Juego.replace('.html', '')
+  Juego = re.split(' vs ',Juego)
 
-tratar = str(tabla[0])
-filas = re.split('\n', tratar)
-print(len(filas))
-i = 0
-# for EEE in filas:
-#   print(i, EEE)
-#   i+=1
-for II in (8,9,13,14,18,19):
-  print(filas[II])
-# print(filas)
-# for link in elemLinks:
-#     elem.append(link.text)
+  
+  tabla = sopa(url, tag, Elemento)
+  tratar = str(tabla[0])
+  filas = re.split('\n', tratar)
+  
+  prediccion = {'J1':Juego[0],
+                'J2':Juego[1],
+                'Gana': filas[9],
+                'Gana 1º Set': filas[14],
+                'Sets': filas[19],
+               }
+  
+  for d in prediccion:
+    dd = str(prediccion[d])
+    dd = dd.replace('<td>', '')
+    dd = dd.replace('</td>', '')
+    prediccion[d] = dd
+  Tabla.append(prediccion)
 
-# if os.path.exists(os.path.join(os.path.dirname(__file__), archivo)):
-#     conf = True
-# else:
-#     os.makedirs(os.path.join(os.path.dirname(__file__), 'CVS Files/'))
-#     conf = False
+col = 20
+llaves = list(Tabla[0].keys())
+print(f"{llaves[0]:<{col}} | {llaves[1]:<{col}} | {llaves[2]:<{col}} | {llaves[3]:<{col}} | {llaves[4]}")
+for fila in Tabla:
+  print(f"{fila[llaves[0]]:<{col}} | {fila[llaves[1]]:<{col}}  | {fila[llaves[2]]:<{col}}  | {fila[llaves[3]]:<{col}} | {fila[llaves[4]]}")
 
+if os.path.exists(os.path.join(os.path.dirname(__file__), archivo)):
+    conf = True
+else:
+    os.makedirs(os.path.join(os.path.dirname(__file__), 'CVS Files/'))
+    conf = False
 
-
-# with open(os.path.join(os.path.dirname(__file__), archivo), 'a', newline='', encoding='utf-8') as f:
-#     writer = csv.writer(f, delimiter=',')
+with open(os.path.join(os.path.dirname(__file__), archivo), 'a', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f, delimiter=',')
     
-#     if conf == False:
-#         writer.writerow(['Equipo 1', 'Equipo 2', 'Ganador', 'Probabilidad', 'Fecha'])
+    if conf == False:
+        writer.writerow([llaves[0], llaves[1], llaves[2], llaves[4], llaves[4]])
     
-#     for partido in encuentros:
-#         writer.writerow([partido['Equipo1'], partido['Equipo2'], partido['Ganador'], partido['Probabilidad'], partido["Fecha"]])
+    for fila in Tabla:
+        writer.writerow([fila[llaves[0]], fila[llaves[1]], fila[llaves[2]], fila[llaves[3]], fila[llaves[4]]])
 
